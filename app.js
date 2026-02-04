163cb779663c6a4819da53f81eeee2f3a698272e
oyinade olawoyin <oyinadeolawoyin@proton.me>
2026-02-04 12:46:25 +0100

    Add frontend https to cors

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://inkwell-inky-three.vercel.app"],
   credentials: true,
 };
 
